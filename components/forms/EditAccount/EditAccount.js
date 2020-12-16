f1651b4410a2969a9f82c6b9492fef72cda736e0
Carlos Martins <luacmartins@gmail.com>
2020-12-16 14:34:58 -0700

    add edit account router and frontend api call

--- components/forms/EditAccount/EditAccount.js
+++ components/forms/EditAccount/EditAccount.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import NumberFormat from 'react-number-format'
+import fetcher from '../../../utils/fetcher'
 import Text from '../../inputs/Text'
 import Select from '../../inputs/Select'
 import Button from '../../shared/Button'
@@ -14,7 +15,7 @@ const types = [
 ]
 
 const EditAccount = ({ data }) => {
-   const [name, setName] = useState('')
+   const [nickname, setNickname] = useState('')
    const [nameError, setNameError] = useState(false)
    const [type, setType] = useState('')
    const [typeError, setTypeError] = useState(false)
@@ -22,15 +23,17 @@ const EditAccount = ({ data }) => {
    const [alert, setAlert] = useState('')
 
    useEffect(() => {
-      setName(data.nickname || '')
+      setNickname(data.nickname || '')
       setType(data.type || 'Cash')
    }, [data])
 
-   const handleSubmit = (e) => {
+   const handleSubmit = (e, id) => {
       e.preventDefault()
       setIsNameLoading(true)
-      setAlert('Name changed!')
-      console.log({ name, type })
+      fetcher.patch(`/api/accounts/${id}`, { nickname, type })
+         .then(res => setAlert('Account updated',))
+         .catch(e => console.log(e.response.data.error))
+      setIsNameLoading(false)
    }
 
    const handleDelete = (e) => {
@@ -42,18 +45,18 @@ const EditAccount = ({ data }) => {
       <>
          <div className="flex flex-col items-center">
             <span className="text-lg text-center">{data.nickname}</span>
-            <span className="text-sm font-normal">{data.name}</span>
+            <span className="text-sm font-normal">{data.nickname}</span>
             <span className="mt-4 text-3xl">
                <NumberFormat value={data.balance} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale={2} fixedDecimalScale={true} />
             </span>
          </div>
          <form className="w-72 md:w-80 mx-auto mt-8">
             <div className="md:hidden">
-               <Text value={name} setValue={setName} error={nameError} setError={setNameError} label={'inside'} labelText={'Account name'} />
+               <Text value={nickname} setValue={setNickname} error={nameError} setError={setNameError} label={'inside'} labelText={'Account nickname'} />
                <Select data={types} value={type} setValue={setType} error={typeError} setError={setTypeError} label={'inside'} labelText={'Account type'} className="mt-4" />
             </div>
             <div className="hidden md:block">
-               <Text value={name} setValue={setName} error={nameError} setError={setNameError} label={'floating'} labelText={'Account name'} />
+               <Text value={nickname} setValue={setNickname} error={nameError} setError={setNameError} label={'floating'} labelText={'Account nickname'} />
                <Select data={types} value={type} setValue={setType} error={typeError} setError={setTypeError} label={'floating'} labelText={'Account type'} className="mt-6" />
             </div>
 
@@ -65,7 +68,7 @@ const EditAccount = ({ data }) => {
                <button onClick={handleDelete} className="text-red-600 underline font-semibold hover:text-red-700">Delete account</button>
                <Button
                   className="block mt-10"
-                  onClick={handleSubmit}
+                  onClick={(e) => handleSubmit(e, data._id)}
                   isLoading={isNameLoading}
                >
                   Done
