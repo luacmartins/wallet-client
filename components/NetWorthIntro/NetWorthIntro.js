fde7d360026add8f9b05410c97a1e40ce47de30c
Carlos Martins <luacmartins@gmail.com>
2020-12-16 13:54:31 -0700

    fix nextjs error with NetWorthIntro component

--- components/NetWorthIntro/NetWorthIntro.js
+++ components/NetWorthIntro/NetWorthIntro.js
@@ -1,11 +1,18 @@
+import { useState, useEffect } from 'react'
 import NumberFormat from 'react-number-format'
 import { useAuth } from '../../utils/AuthProvider'
 
 const NetWorthIntro = ({ data }) => {
    const { user } = useAuth()
+   const [username, setUsername] = useState('')
+
+   useEffect(() => {
+      setUsername(user)
+   }, [user])
+
    return (
       <div className="flex flex-col items-center">
-         <span className="text-sm">Hello {user}, your net worth is</span>
+         <span className="text-sm">Hello {username}, your net worth is</span>
          <span className="text-3xl font-bold">
             <NumberFormat value={data.amount / 100} displayType={'text'} thousandSeparator={true} prefix={'$'} />
          </span>
