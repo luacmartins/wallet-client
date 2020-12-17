539950bfbb862f27d01586f95ee35edf8e5e3130
Carlos Martins <luacmartins@gmail.com>
2020-12-16 18:58:27 -0700

    fix amount display on edit transactions

--- components/desktop/EditTransaction/EditTransaction.js
+++ components/desktop/EditTransaction/EditTransaction.js
@@ -6,7 +6,7 @@ import Button from '../../shared/Button'
 import Amount from '../../inputs/Amount'
 
 const EditTransaction = ({ data }) => {
-   const date = moment(data.date.user || data.date.original).format('MMM, D YYYY')
+   const date = data.date.user || data.date.original
    const originalDate = moment(data.date.original).format('MMM, D YYYY')
    const description = data.description.user || data.description.original
 
@@ -16,12 +16,12 @@ const EditTransaction = ({ data }) => {
             <div className="grid grid-cols-4 grid-rows-2 gap-4 ">
                <Date labelText={'Date'} label={'floating'} background={'bg-theme-gray-200'} value={date} />
                <div className="col-span-2">
-                  <Text labelText={'Description'} label={'floating'} background={'bg-theme-gray-200'} value={description} />
+                  <Text labelText={'Description'} label={'floating'} background={'bg-theme-gray-200'} value={description.toLowerCase()} />
                </div>
-               <Amount labelText={'Amount'} label={'floating'} background={'bg-theme-gray-200'} value={data.amount / 100} />
-               <Select labelText={'Type'} label={'floating'} data={[]} background={'bg-theme-gray-200'} />
+               <Amount labelText={'Amount'} label={'floating'} background={'bg-theme-gray-200'} value={data.amount} />
+               <Select labelText={'Type'} label={'floating'} data={[]} background={'bg-theme-gray-200'} data={[{ text: 'Debit', value: 'debit' }, { text: 'Credit', value: 'credit' }]} />
                <div className="col-span-2">
-                  <Select labelText={'Category'} label={'floating'} data={[]} background={'bg-theme-gray-200'} value={data.category} />
+                  <Select labelText={'Category'} label={'floating'} data={[]} background={'bg-theme-gray-200'} value={data.category} data={[{ text: 'Accommodation', value: 'accommodation' }, { text: 'Transportation', value: 'transportation' }]} />
                </div>
                <Button className="w-full">Done</Button>
             </div>
--- components/mobile/EditTransaction/EditTransaction.js
+++ components/mobile/EditTransaction/EditTransaction.js
@@ -1,16 +1,20 @@
 import NumberFormat from 'react-number-format'
+import moment from 'moment'
 import Select from '../../inputs/Select'
 import Text from '../../inputs/Text'
 import Date from '../../inputs/Date'
 import Button from '../../shared/Button'
 
 const EditTransaction = ({ data }) => {
+   const description = data.description.user || data.description.original
+   const date = data.date.user || data.date.original
+
    return (
       <div className={`flex flex-col items-center`}>
          <span className="text-sm text-theme-gray-700 font-normal">{data.account}</span>
          <NumberFormat
             name="amount"
-            value={data.amount / 100}
+            value={data.amount}
             displayType={'text'}
             thousandSeparator={true}
             decimalScale={2}
@@ -19,13 +23,13 @@ const EditTransaction = ({ data }) => {
             defaultValue={0}
             className="bg-transparent text-center text-4xl font-semibold w-full my-2"
          />
-         <span className="">{data.description.user || data.description.original}</span>
-         <span className="font-normal text-sm">{data.date.user || data.date.original}</span>
+         <span className="capitalize">{description.toLowerCase()}</span>
+         <span className="font-normal text-sm">{moment(date).format('MMM D, YYYY')}</span>
          <div className="flex flex-col gap-y-2 w-full mt-8">
-            <Text label={'inside'} labelText={'Description'} value={data.description.user || data.description.original} />
-            <Date label={'inside'} labelText={'Date'} value={data.date.user || data.date.original} />
+            <Text label={'inside'} labelText={'Description'} value={description.toLowerCase()} />
+            <Date label={'inside'} labelText={'Date'} value={date} />
             <Select label={'inside'} labelText={'Category'} value={data.category} data={[{ text: 'Accommodation', value: 'accommodation' }, { text: 'Transportation', value: 'transportation' }]} />
-            <span className="font-normal text-sm text-theme-gray-700 mt-2">Appears on your statement as {data.description.original}, {data.date.original}</span>
+            <span className="font-normal text-sm text-theme-gray-700 mt-2">Appears on your {data.account} statement as {data.description.original} on {moment(data.date.original).format('MMM D, YYYY')}</span>
             <div className="flex justify-center">
                <Button className="mt-6">Done</Button>
             </div>
