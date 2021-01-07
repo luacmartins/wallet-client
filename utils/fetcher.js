2b6947ad0187dbcb8f273d75dd5452974c76d14f
Carlos Martins <luacmartins@gmail.com>
2021-01-07 14:34:58 -0700

    add axios interceptor to fetch token cookie

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -8,9 +8,16 @@ const fetcher = axios.create({
    baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL}`
 })
 
-if (token) {
-   fetcher.defaults.headers.common['Authorization'] = token
-}
-
+fetcher.interceptors.request.use(
+   config => {
+      const token = cookies.get('token');
+      if (token) {
+         config.headers['Authorization'] = token;
+      }
+      return config;
+   },
+   error => {
+      Promise.reject(error)
+   })
 
 export default fetcher
