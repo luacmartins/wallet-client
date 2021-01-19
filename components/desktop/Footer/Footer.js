e04ee811b1348a1b32fe585d63eead28a78064e5
Carlos Martins <luacmartins@gmail.com>
2021-01-19 13:39:31 -0700

    add dynamic date to footer

--- components/desktop/Footer/Footer.js
+++ components/desktop/Footer/Footer.js
@@ -1,7 +1,9 @@
+import moment from 'moment'
+
 const Footer = () => {
    return (
       <footer className="hidden md:flex mt-20 justify-center items-center h-16 bg-theme-beige-900 w-full  text-white font-normal">
-         <span>Wallet - 2020</span>
+         <span>Wallet - {moment().format('YYYY')}</span>
       </footer>
    );
 }
