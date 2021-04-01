21d237229fcf8cadd4445e07d09eab9f37ef67cc
Carlos Martins <luacmartins@gmail.com>
2021-04-01 10:08:36 -0600

    add spinner to guest login

--- components/forms/Login/Login.js
+++ components/forms/Login/Login.js
@@ -3,43 +3,68 @@ import { useForm } from 'react-hook-form'
 import { useAuth } from '../../../utils/useAPI'
 import useValidation from '../../../utils/useValidation'
 import { Logo } from '../../icons'
-import { Button, Alert } from '../../shared'
+import { Button, Alert, Spinner } from '../../shared'
 import Input from '../../inputs/Input'
 
 const Login = () => {
-   const { register, errors, handleSubmit, formState: { isSubmitting } } = useForm()
-   const validation = useValidation()
-   const { alert, loginUser, loginGuest } = useAuth()
+  const {
+    register,
+    errors,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm()
+  const validation = useValidation()
+  const { alert, isLoading, loginUser, loginGuest } = useAuth()
 
-   return (
-      <div className="w-64 md:w-80 mx-auto">
-         <div className="mb-8 text-center">
-            <Logo className="w-16 mx-auto" />
-            <header className="text-theme-yellow-500 text-2xl">Wallet</header>
-         </div>
-
-         <form onSubmit={handleSubmit(loginUser)} className="grid gap-y-2" noValidate>
-            <Alert data={alert} />
-            <Input type={'email'} name={'email'} placeholder={'Email'} error={errors} register={register(validation.email)} />
-            <Input type={'password'} name={'password'} placeholder={'Password'} error={errors} register={register(validation.password)} />
-            <Link href="/user/forgot-password">
-               <a className="font-thin text-sm underline">Forgot your password?</a>
-            </Link>
-            <Button className="block mt-4 mx-auto" isLoading={isSubmitting} type={'submit'}>Login</Button>
-         </form>
-
-         <div className="text-center mt-8">
-            <span className="font-thin">Don't have an account?</span>
-            <Link href="/user/sign-up">
-               <a className="text-theme-yellow-500 underline ml-1 hover:text-theme-yellow-700">Sign up</a>
-            </Link>
-            <span className="font-thin block my-2">or</span>
-            <button onClick={loginGuest} className="text-theme-yellow-500 underline ml-1 font-semibold hover:text-theme-yellow-700">
-               View as guest
-               </button>
-         </div>
+  return (
+    <div className='w-64 mx-auto md:w-80'>
+      <div className='mb-8 text-center'>
+        <Logo className='w-16 mx-auto' />
+        <header className='text-2xl text-theme-yellow-500'>Wallet</header>
       </div>
-   )
+
+      <form onSubmit={handleSubmit(loginUser)} className='grid gap-y-2' noValidate>
+        <Alert data={alert} />
+        <Input
+          type={'email'}
+          name={'email'}
+          placeholder={'Email'}
+          error={errors}
+          register={register(validation.email)}
+        />
+        <Input
+          type={'password'}
+          name={'password'}
+          placeholder={'Password'}
+          error={errors}
+          register={register(validation.password)}
+        />
+        <Link href='/user/forgot-password'>
+          <a className='text-sm font-thin underline'>Forgot your password?</a>
+        </Link>
+        <Button className='block mx-auto mt-4' isLoading={isSubmitting} type={'submit'}>
+          Login
+        </Button>
+      </form>
+
+      <div className='mt-8 text-center'>
+        <span className='font-thin'>Don't have an account?</span>
+        <Link href='/user/sign-up'>
+          <a className='ml-1 underline text-theme-yellow-500 hover:text-theme-yellow-700'>
+            Sign up
+          </a>
+        </Link>
+        <span className='block my-2 font-thin'>or</span>
+        <button
+          onClick={loginGuest}
+          className='ml-1 font-semibold underline text-theme-yellow-500 hover:text-theme-yellow-700'
+        >
+          View as guest
+        </button>
+        {isLoading && <Spinner />}
+      </div>
+    </div>
+  )
 }
 
-export default Login;
+export default Login
--- utils/useAPI/useAuth/useAuth.js
+++ utils/useAPI/useAuth/useAuth.js
@@ -4,82 +4,107 @@ import { useRouter } from 'next/router'
 import { fetcher } from '../config'
 import useAlert from '../../useAlert'
 
-
 const useAuth = () => {
-   const cookies = new Cookies()
-   const router = useRouter()
-   const { alert, flash } = useAlert()
+  const cookies = new Cookies()
+  const router = useRouter()
+  const { alert, flash } = useAlert()
 
-   const [username, setUsername] = useState('')
+  const [username, setUsername] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
-   useEffect(() => {
-      setUsername(cookies.get('user'))
-   }, [])
+  useEffect(() => {
+    setUsername(cookies.get('user'))
+  }, [])
 
-
-   const updatePassword = (data) => {
-      fetcher.patch('/api/change-password', data)
-         .then(() => {
-            flash({ type: 'success', message: 'Your password was successfully updated!' })
-         })
-         .catch(e => {
-            flash({ type: 'error', message: 'Your current password is incorrect.' })
-         })
-   }
-
-   const loginUser = (data) => {
-      fetcher.post('/api/login', data)
-         .then(res => {
-            cookies.set('token', res.data.token)
-            cookies.set('user', res.data.user)
-            router.push('/dashboard')
-         }).catch(() => {
-            flash({ type: 'error', message: 'Invalid credentials. Please try again.' })
-         })
-   }
-
-   const loginGuest = () => {
-      fetcher.post('/api/login', { email: 'guest@example.com', password: '1234abcd' })
-         .then(res => {
-            cookies.set('token', res.data.token)
-            cookies.set('user', res.data.user)
-            router.push('/dashboard')
-         }).catch(() => {
-            flash({ type: 'error', message: 'There was a problem loggin in. Please try again later.' })
-         })
-   }
-
-   const signUp = (data) => {
-      fetcher.post('/api/signup', data).then(res => {
-         cookies.set('token', res.data.token)
-         cookies.set('user', res.data.user)
-         router.push('/dashboard')
-      }).catch(e => {
-         flash({ type: 'error', message: 'Unable to sign up. Please try again later.' })
+  const updatePassword = data => {
+    fetcher
+      .patch('/api/change-password', data)
+      .then(() => {
+        flash({ type: 'success', message: 'Your password was successfully updated!' })
       })
-   }
+      .catch(e => {
+        flash({ type: 'error', message: 'Your current password is incorrect.' })
+      })
+  }
 
-   const logout = () => {
-      fetcher.get('/api/logout')
-         .then(() => {
-            cookies.remove('token')
-            cookies.remove('user')
-            router.push('/')
-         })
-         .catch(e => console.log(e))
-   }
+  const loginUser = data => {
+    setIsLoading(true)
+    fetcher
+      .post('/api/login', data)
+      .then(res => {
+        cookies.set('token', res.data.token)
+        cookies.set('user', res.data.user)
+        setIsLoading(false)
+        router.push('/dashboard')
+      })
+      .catch(() => {
+        setIsLoading(false)
+        flash({ type: 'error', message: 'Invalid credentials. Please try again.' })
+      })
+  }
 
-   const forgotPassword = (data) => {
-      // fill rest of function
-      console.log(data)
-   }
+  const loginGuest = () => {
+    setIsLoading(true)
+    fetcher
+      .post('/api/login', { email: 'guest@example.com', password: '1234abcd' })
+      .then(res => {
+        cookies.set('token', res.data.token)
+        cookies.set('user', res.data.user)
+        setIsLoading(false)
+        router.push('/dashboard')
+      })
+      .catch(() => {
+        setIsLoading(false)
+        flash({ type: 'error', message: 'There was a problem loggin in. Please try again later.' })
+      })
+  }
 
-   const resetPassword = (data) => {
-      // fill rest of function
-      console.log(data)
-   }
+  const signUp = data => {
+    fetcher
+      .post('/api/signup', data)
+      .then(res => {
+        cookies.set('token', res.data.token)
+        cookies.set('user', res.data.user)
+        router.push('/dashboard')
+      })
+      .catch(e => {
+        flash({ type: 'error', message: 'Unable to sign up. Please try again later.' })
+      })
+  }
 
-   return { username, alert, updatePassword, loginUser, loginGuest, signUp, forgotPassword, resetPassword, logout }
+  const logout = () => {
+    fetcher
+      .get('/api/logout')
+      .then(() => {
+        cookies.remove('token')
+        cookies.remove('user')
+        router.push('/')
+      })
+      .catch(e => console.log(e))
+  }
+
+  const forgotPassword = data => {
+    // fill rest of function
+    console.log(data)
+  }
+
+  const resetPassword = data => {
+    // fill rest of function
+    console.log(data)
+  }
+
+  return {
+    username,
+    alert,
+    isLoading,
+    updatePassword,
+    loginUser,
+    loginGuest,
+    signUp,
+    forgotPassword,
+    resetPassword,
+    logout,
+  }
 }
 
 export default useAuth
